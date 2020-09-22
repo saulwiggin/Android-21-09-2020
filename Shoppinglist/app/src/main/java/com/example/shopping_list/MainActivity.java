1371b233f531fae6f1a5fa1eb58b9ae806d7091b
saul.wiggin <saul.wiggin@retrospect.com>
2020-09-22 16:49:17 +0100

    shopping list

--- Shoppinglist/app/src/main/java/com/example/shopping_list/MainActivity.java
+++ Shoppinglist/app/src/main/java/com/example/shopping_list/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.shopping_list;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView textview;
     TextView textview2;
@@ -21,21 +24,59 @@ public class MainActivity extends AppCompatActivity {
     TextView textview9;
     TextView textview10;
 
+    private EditText mLocationEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLocationEditText = findViewById(R.id.plain_text_input);
+
     }
 
-    Button button = (Button) findViewById(R.id.additembutton);
-    button.setOnClickListener(new OnClickListener() {
-        public void onClick(View v) {
-            // Do something in response to button click
-            Intent intent = new Intent(this, secondActivity.class);
-            startActivity(intent);
+    @Override
+    public void onClick(View view) {
+        Intent intent=null;
+        View addButton = findViewById(R.id.additembutton);
 
+        switch (view.getId()){
+            case R.id.additembutton:
+                /**
+                 * Action
+                 * Data
+                 */
+                intent = new Intent(this, secondActivity.class);
+                startActivity(intent);
+                break;
         }
-    });
+        if(intent!=null)
+            startActivity(intent);
+    }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if(resultCode == RESULT_OK) {
+                String strEditText = data.getStringExtra("editTextValue");
+            }
+        }
+    }
+
+
+    public void Locate(View view) {
+        // GET THE STRING FROM THE EDIT TEXT FIELD
+        String loc = mLocationEditText.getText().toString();
+
+        // PARSE THE GOOGLE LOCATION STRING INTO AN INTENT
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
 
 }
--- Shoppinglist/app/src/main/java/com/example/shopping_list/secondActivity.java
+++ Shoppinglist/app/src/main/java/com/example/shopping_list/secondActivity.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class secondActivity extends AppCompatActivity {
+public class secondActivity extends AppCompatActivity implements View.OnClickListener {
 
     /*
     * import buttons here
@@ -29,8 +30,91 @@ public class secondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = findViewById(R.id.text_message);
-        textView.setText(message);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent=null;
+        View Button2 = findViewById(R.id.button2);
+        String input2 = Button2.toString();
+        View Button3 = findViewById(R.id.button3);
+        String input3 = Button3.toString();
+        View Button4 = findViewById(R.id.button4);
+        String input4 = Button2.toString();
+        View Button5 = findViewById(R.id.button5);
+        String input5 = Button5.toString();
+        View Button6 = findViewById(R.id.button6);
+        String input6 = Button6.toString();
+        View Button7 = findViewById(R.id.button7);
+        String input7 = Button7.toString();
+        View Button8 = findViewById(R.id.button8);
+        String input8 = Button8.toString();
+        View Button9 = findViewById(R.id.button9);
+        String input9 = Button9.toString();
+        View Button10 = findViewById(R.id.button10);
+        String input10 = Button10.toString();
+        View Button11 = findViewById(R.id.button11);
+        String input11 = Button11.toString();
+        View Button12 = findViewById(R.id.button12);
+        String input12 = Button12.toString();
+        switch (view.getId()){
+            case R.id.button2:
+                intent = new Intent(this, MainActivity.class);
+                intent.putExtra("ShoppingItem", input2);
+                startActivity(intent);
+                break;
+            case R.id.button3:
+                intent = new Intent(this, MainActivity.class);
+                intent.putExtra("ShoppingItem", input3);
+                startActivity(intent);
+                break;
+            case R.id.button4:
+                intent = new Intent(this, MainActivity.class);
+                intent.putExtra("ShoppingItem", input4);
+                startActivity(intent);
+                break;
+            case R.id.button5:
+                intent = new Intent(this, MainActivity.class);
+                intent.putExtra("ShoppingItem", input5);
+                startActivity(intent);
+                break;
+            case R.id.button6:
+                intent = new Intent(this, MainActivity.class);
+                intent.putExtra("ShoppingItem", input6);
+                startActivity(intent);
+                break;
+            case R.id.button7:
+                intent = new Intent(this, MainActivity.class);
+                intent.putExtra("ShoppingItem", input7);
+                startActivity(intent);
+                break;
+            case R.id.button8:
+                intent = new Intent(this, MainActivity.class);
+                intent.putExtra("ShoppingItem", input8);
+                startActivity(intent);
+                break;
+            case R.id.button9:
+                intent = new Intent(this, MainActivity.class);
+                intent.putExtra("ShoppingItem", input9);
+                startActivity(intent);
+                break;
+            case R.id.button10:
+                intent = new Intent(this, MainActivity.class);
+                intent.putExtra("ShoppingItem", input10);
+                startActivity(intent);
+                break;
+            case R.id.button11:
+                intent = new Intent(this, MainActivity.class);
+                intent.putExtra("ShoppingItem", input11);
+                startActivity(intent);
+                break;
+            case R.id.button12:
+                intent = new Intent(this, MainActivity.class);
+                intent.putExtra("ShoppingItem", input12);
+                startActivity(intent);
+                break;
+        }
+        if(intent!=null)
+            startActivity(intent);
     }
 }
